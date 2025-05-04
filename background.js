c5ae11a1f9a3e39de2e4bafc3285c86491861ea2
Nguyễn Văn Duy <duynv@c3tek.biz>
2025-05-04 16:25:07 +0700

    update more

--- background.js
+++ background.js
@@ -24,7 +24,7 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.alarms.create("loginExecute",
         {
             delayInMinutes: 0,
-            periodInMinutes: 30
+            periodInMinutes: 45
         });
 });
 
